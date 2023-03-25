05ed867e5e55fb4464aba8f172cf3f1fefa092dc
Nathan Bleigh <nathan.bleigh@gmail.com>
2023-03-25 13:47:06 -0500

    other dead code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,12 @@
-import { Layout, Text, Code, Page, Link } from "@vercel/examples-ui";
-
-export default function Home({ data }: { data: any }) {
+export default function Home() {
   return (
-    <Page>
+    <div>
       <h1>Build for Each Other</h1>
       <p>Build for each other, and promote your peers using rocket PRs.</p>
       <p>
         Every week, this will post a report of the previous week's PRs merged
         with a 🚀 reaction from at least one colleague.
       </p>
-    </Page>
+    </div>
   );
 }
-
-Home.Layout = Layout;
